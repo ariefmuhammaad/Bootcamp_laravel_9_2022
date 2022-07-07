5cb2a64896719f560975839fa7dc342bd44746f0
ariefmuhammaad <107197767+ariefmuhammaad@users.noreply.github.com>
2022-07-07 08:11:51 +0700

    backsite frontsite

---
+++ app/Http/Controllers/Frontsite/RegisterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Frontsite;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+}
